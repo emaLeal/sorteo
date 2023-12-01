9a5db58ca3772b876527d7c337d0493adb066671
Leal31 <lealemanuel31@protonmail.com>
2023-12-01 11:19:18 -0500

    optimizacion

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+  webpack: (config) => {
+    return { ...config, optimization: { minimize: false } };
+  },
 };
 
 module.exports = nextConfig;
