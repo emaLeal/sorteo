206e2d7f121814cedbe901b0ce56829d8555465d
Leal31 <lealemanuel31@protonmail.com>
2024-08-06 15:31:40 -0500

    Ms

--- src/app/api/historial/[id]/route.js
+++ src/app/api/historial/[id]/route.js
@@ -12,7 +12,7 @@ export async function GET(req, params) {
     s.nombre
     from historial h inner join participantes p on h.evento_id=p.evento_id
     inner join sorteos s on s.evento_id=p.evento_id
-    inner join evento e on e.id=p.evento_id where e.id=?
+    inner join evento e on e.id=p.evento_id where h.evento_id=?
     `,
     values: [id],
   });
