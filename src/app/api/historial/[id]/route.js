128483959b1c1e5ecd4b59a243b3344f7ad55bba
Leal31 <lealemanuel31@protonmail.com>
2024-08-08 08:50:22 -0500

    ms

--- src/app/api/historial/[id]/route.js
+++ src/app/api/historial/[id]/route.js
@@ -22,27 +22,38 @@ export async function GET(req, params) {
 
 export async function POST(req, params) {
   const body = await req.json();
+  const select =
+    "select * from historial where not exists (select 1 from historial where participante_id=? and evento_id=?";
 
-  try {
-    const query =
-      "sorteo_id" in body
-        ? "insert into historial (evento_id, sorteo_id, participante_id) values (?,?,?)"
-        : "insert into historial (evento_id, participante_id) values (?,?)";
-    let insert;
-    if ("sorteo_id" in body) {
-      insert = await executeQuery({
-        query,
-        values: [body.evento_id, body.sorteo_id, body.participante_id],
-      });
-    } else {
-      insert = await executeQuery({
-        query,
-        values: [body.evento_id, body.participante_id],
-      });
+  const exists = await executeQuery({
+    query: select,
+    values: [body.participante_id, body.evento_id],
+  });
+  console.log(exists);
+  if (exists.length === 0) {
+    try {
+      const query =
+        "sorteo_id" in body
+          ? "insert into historial (evento_id, sorteo_id, participante_id) values (?,?,?)"
+          : "insert into historial (evento_id, participante_id) values (?,?)";
+      let insert;
+      if ("sorteo_id" in body) {
+        insert = await executeQuery({
+          query,
+          values: [body.evento_id, body.sorteo_id, body.participante_id],
+        });
+      } else {
+        insert = await executeQuery({
+          query,
+          values: [body.evento_id, body.participante_id],
+        });
+      }
+      return NextResponse.json({ message: "Ok" }, { status: 201 });
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ error }, { status: 400 });
     }
-    return NextResponse.json({ message: "Ok" }, { status: 201 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error }, { status: 400 });
+  } else {
+    return NextResponse.json({ message: "Ya existe" }, { status: 400 });
   }
 }
